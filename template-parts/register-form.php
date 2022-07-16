754decf03d97e9aef2463ad20ef505e815de6845
Yevhen Zakarliuka <ifnullez@gmail.com>
2022-07-16 14:07:38 +0300

    chenge register form lost password link

--- template-parts/register-form.php
+++ template-parts/register-form.php
@@ -8,9 +8,9 @@
         <input type="password" class="form-control" id="user_pass" name="user_pass" placeholder="<?php _e('Password', 'woocommerce'); ?>">
         <label for="user_pass"><?php _e('Password', 'woocommerce'); ?></label>
     </div>
-    <button type="button" id="forgot_password" class="forgot_button">
+    <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>" id="forgot_password" class="forgot_button">
         <?php _e('Forgot password', 'woocommerce'); ?>
-    </button>
+    </a>
     <button type="submit" id="register_submit" name="login_submit" class="btn btn-success w-100 mt-3"><?php _e('Register', 'woocommerce'); ?></button>
     <?php wp_nonce_field( 'register_nonce_security' ); ?>
 </form>
